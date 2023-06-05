18557f324c36eac27b82479ef6ade2c8adc764b8
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-06-05 21:48:12 +0000

    Register Page Created

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 type LoginProps = {
-    
+
 };
 
-const Login:React.FC<LoginProps> = () => {
-    
-    return(
+const Login: React.FC<LoginProps> = () => {
+
+    return (
         <form className='space-y-6 px-6 pb-4'>
             <h3 className='text-xl font-medium text-white'>Sign Into LeetClone</h3>
             <div>
@@ -22,7 +22,7 @@ const Login:React.FC<LoginProps> = () => {
                        block w-full p-2.5 bg-gray-600 placeholder-gray-500 text-white
                     '
                     placeholder='youremail@email.com'
-                  />
+                />
             </div>
             <div>
                 <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
@@ -37,25 +37,25 @@ const Login:React.FC<LoginProps> = () => {
                        block w-full p-2.5 bg-gray-600 placeholder-gray-500 text-white
                     '
                     placeholder='************************'
-                  />
-                  <button 
-                        type='submit'
-                        className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
+                />
+                <button
+                    type='submit'
+                    className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
                         text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s mt-10'
-                        >
-                      Login
-                  </button>
-                  <button className='flex w-full justify-end'>
+                >
+                    Login
+                </button>
+                <button className='flex w-full justify-end'>
                     <a href='' className='text-sm block text-brand-orange hover:underline w-full text-right'>
                         Forgot password?
                     </a>
-                  </button>
-                  <div className='text-sm font-medium text-gray-300 mt-10'>
-                        Not Registered? {" "}
-                        <a href='#' className='text-blue-700 hover-underline'>
-                            Create Account
-                        </a>
-                  </div>
+                </button>
+                <div className='text-sm font-medium text-gray-300 mt-10'>
+                    Not Registered? {" "}
+                    <a href='#' className='text-blue-700 hover-underline'>
+                        Create Account
+                    </a>
+                </div>
             </div>
         </form>
     );
